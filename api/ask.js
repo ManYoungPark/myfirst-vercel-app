957669a028a8ceadff45213d1890c76e9494a256
ManYoungPark <pmy10042@gmail.com>
2025-04-28 14:29:56 +0900

    'second'

--- api/ask.js
+++ api/ask.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
     if (req.method === 'POST') {
       const { question } = req.body;
       console.log('받은 질문:', question);
-      res.status(200).json({ message: '질문이 잘 접수되었습니다!' });
+      res.status(200).json({ message: '질문이 잘 접수되었습니다!두번째' });
     } else {
       res.status(405).json({ message: '허용되지 않는 요청입니다.' });
     }
